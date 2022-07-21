2a727d4affcd61f01112e4cd612b4f7f68a8fa8a
uthso007 <102689737+uthso007@users.noreply.github.com>
2022-07-21 22:44:34 +0600

    Add files via upload

---
+++ Average_Of_two_num.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float num1,num2,sum,average;
+    cout<<"Enter two number"<<endl;
+    cin>>num1>>num2;
+    sum=num1+num2;
+    average=sum/2;
+    cout<<"The sum of the two number is "<<sum<<endl;
+    cout<<"The average of the two number is "<<average<<endl;
+    return 0;
+}
